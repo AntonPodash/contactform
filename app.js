4f0c7731eccd4d61cc40b4e9b0bb316fd92eb41b
AntonPodash <apodash01@gmail.com>
2019-06-11 13:59:30 +0300

    last version

---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index.html', (req, res) => {
+});
+
+app.post('/send', urlencodedParser, (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+  `;
+    
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'contactformua@gmail.com',
+            pass: 'kpi-ip85'
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    });
+    
+    const mailoptions = {
+      from: 'contactformua@gmail.com', 
+      to: 'contactformua@gmail.com', 
+      subject: 'Contact Request',
+      html: output
+    };
+    
+    transporter.sendMail(mailoptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+    });
+    res.redirect(req.get('referer'));
+});
+
+app.listen(8000, () => console.log('Server started'))
